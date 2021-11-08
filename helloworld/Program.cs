dc3332e3d833fafe455f90d9860a89198819c455
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2021-11-08 14:32:43 +0300

    Hello World & NameSurname

---
+++ helloworld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace netcore
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisiminizi giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine($"Merhaba {name} {surname}");
+            
+            Console.ReadKey();
+        }
+    }
+}
