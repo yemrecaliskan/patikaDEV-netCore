2f6506a10b808292337bf1fc973a0d389e7606ca
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2021-11-10 16:00:20 +0300

    add diziler

---
+++ diziler/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi Tanımlama
+            string[] renkler = new string[5];
+            string[] hayvanlar = {"Kedi","Köpek","Kuş","Maymun"};
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //Dizilere Değer atama ve erişim
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //Döngüler dizi kullanımı
+            //Klavyeden girilen n tane sayının ortalamasını hesaplayan program
+            Console.Write("Lütfen dizinin eleman sayısını giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayısını giriniz: ", i + 1);  
+                sayiDizisi[i] = int.Parse(Console.ReadLine()); 
+            }
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+                toplam += sayi;
+            Console.WriteLine("Ortalama: " + toplam / diziUzunlugu);
+
+            Console.ReadKey();
+
+        }
+    }
+}
