ef201bb165ec5a052502033f461d26f0686f8ee7
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2021-11-10 14:55:01 +0300

    add donguler-for-loop

---
+++ donguler-for-loop/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ekrandan girilen sayıya kadar olan tek sayıları ekrana yazdır.
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayac; i++)
+            {
+                //komutlar
+                if (i%2 == 1)
+                    Console.WriteLine(i);
+            }
+            
+            //1 ile 1000 arasındaki tek ve çift sayıların toplamlarını ekrana yazdır.
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if(i%2 == 1)
+                    tekToplam +=i; //tekToplam = tekToplam +i;
+                else
+                    ciftToplam +=i; //ciftToplam = ciftToplam +i;
+            }
+            Console.WriteLine("Tek Toplam: " + tekToplam);
+            Console.WriteLine("Çift Toplam: " + ciftToplam);
+
+            // break, continue
+
+            for (int i = 1; i < 10; i++)
+            {
+                if(i == 4)
+                    break;
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                if(i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
